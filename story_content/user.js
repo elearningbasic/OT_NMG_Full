ae796688039f428c4e88453980effdb215270f47
elearningbasic <elearning.basic@gmail.com>
2024-01-23 20:21:12 +0300

    3

--- story_content/user.js
+++ story_content/user.js
@@ -2,50 +2,6 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6C9elrBVdzM":
-        Script1();
-        break;
-      case "6HLaebNjun7":
-        Script2();
-        break;
-      case "63t92IgWkFf":
-        Script3();
-        break;
-      case "5r0DrGPScBW":
-        Script4();
-        break;
   }
-}
-
-function Script1()
-{
-  let allVideosInCourse = document.getElementsByTagName("video"); 
-for (let i = 0; i < allVideosInCourse.length; i++) {
-  allVideosInCourse[i].setAttribute("controls", "true");
-}
-}
-
-function Script2()
-{
-  let allVideosInCourse = document.getElementsByTagName("video"); 
-for (let i = 0; i < allVideosInCourse.length; i++) {
-  allVideosInCourse[i].setAttribute("controls", "true");
-}
-}
-
-function Script3()
-{
-  let allVideosInCourse = document.getElementsByTagName("video"); 
-for (let i = 0; i < allVideosInCourse.length; i++) {
-  allVideosInCourse[i].setAttribute("controls", "true");
-}
-}
-
-function Script4()
-{
-  let allVideosInCourse = document.getElementsByTagName("video"); 
-for (let i = 0; i < allVideosInCourse.length; i++) {
-  allVideosInCourse[i].setAttribute("controls", "true");
-}
 }
 
